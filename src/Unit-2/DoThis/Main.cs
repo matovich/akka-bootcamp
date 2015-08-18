3b29de6e9700502e6593f03bd50bb520291b1f74
matovich <matovich@hotmail.com>
2015-08-17 21:07:45 -0500

    Start unit 2 lesson 3

--- src/Unit-2/DoThis/Main.cs
+++ src/Unit-2/DoThis/Main.cs
@@ -33,10 +33,20 @@ namespace ChartApp
 
         #endregion
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnCpu_Click(object sender, EventArgs e)
         {
-            var series = ChartDataHelper.RandomSeries("FakeSeries" + _seriesCounter.GetAndIncrement());
-            _chartActor.Tell(new ChartingActor.AddSeries(series));
+
         }
+
+        private void btnMemory_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDisk_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
